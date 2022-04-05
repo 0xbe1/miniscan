744ae065e3ff1fdad3c8bd3de4620d53e686bd54
0xbe1 <0xbetrue@gmail.com>
2022-04-05 22:31:54 +0800

    it works

---
+++ pages/api/startblock.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Result } from '..'
+import axios from 'axios'
+
+const API_TIMEOUT = 5000
+
+// TODO: response type
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Result>
+) {
+  const address = req.query['address'] as string
+  try {
+    const { data } = await axios.get(
+      `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=1&sort=asc&apikey=${process.env.ETHERSCAN_API_KEY}`,
+      {
+        timeout: API_TIMEOUT,
+      }
+    )
+    if (data.status === '1') {
+      res.status(200).json({
+        data: {
+          blockNumber: data.result[0].blockNumber,
+        },
+      })
+    } else {
+      res.status(200).json({
+        data: {
+          blockNumber: 0,
+        },
+        error: {
+          msg: data.message,
+        },
+      })
+    }
+  } catch (error: any) {
+    // TODO: log error
+    res.status(500).json({
+      data: {
+        blockNumber: 0,
+      },
+      error: {
+        msg: 'unknown error',
+      },
+    })
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,126 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export type Result = {
+  data: {
+    blockNumber: number
+  }
+  error?: {
+    msg: string
+  }
+}
+
+function validateAddress(address: string): boolean {
+  return /^0x[0-9a-fA-F]{40}$/.test(address)
+}
+
+const Answer = ({ address, loading, result }: { address: string, loading: boolean, result: Result | null }) => {
+  if (!address) {
+    return <p>Try it 👆</p>
+  }
+  if (!validateAddress(address)) {
+    return <p>Invalid address ❌</p>
+  }
+  if (loading) {
+    return <p>loading... ⏳</p>
+  }
+  if (!result) {
+    return <p>no data ❌</p>
+  }
+  if (result.error) {
+    return <p>{result.error.msg} ❌</p>
+  }
+  return <p className="text-purple-600">{result.data.blockNumber}</p>
+}
 
 const Home: NextPage = () => {
+  // autofocus input, see https://reactjs.org/docs/hooks-reference.html#useref
+  const inputElement = React.useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    if (inputElement.current) {
+      inputElement.current.focus()
+    }
+  }, [])
+
+  const [wipAddress, setWipAddress] = useState('')
+  const [address, setAddress] = useState(wipAddress)
+  const [result, setResult] = useState<Result | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true)
+      try {
+        const { data } = await axios.get(`api/startblock?address=${address}`)
+        setResult(data)
+      } catch (error: any) {
+        setResult(null)
+      }
+      setLoading(false)
+    }
+    if (validateAddress(address)) {
+      fetchData()
+    }
+  }, [address])
+
+  function handleChange(event: React.FormEvent<HTMLInputElement>) {
+    setWipAddress(event.currentTarget.value)
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLInputElement>) {
+    event.preventDefault()
+    setAddress(wipAddress)
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center font-mono">
       <Head>
-        <title>Create Next App</title>
+        <title>startblock</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+      <main className="flex w-full flex-1 items-center sm:w-2/3 lg:w-1/3">
+        <div className="w-full">
+          <div className=" text-center">
+            <p className="bg-gradient-to-tr from-purple-600 to-blue-600 bg-clip-text text-6xl font-bold text-transparent">
+              startblock
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+            <p className="mt-5 text-xl">
+              Find a contract's startblock{' '}
+              <span className="font-bold text-purple-600">in 1 sec</span>{' '}
             </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </div>
+          <input
+            type="text"
+            className="form-control relative my-7 block w-full min-w-0 flex-auto rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-center text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+            placeholder="contract address"
+            aria-label="Search"
+            aria-describedby="button-addon2"
+            value={wipAddress}
+            onChange={handleChange}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleSubmit(e)
+              }
+            }}
+            ref={inputElement}
+          />
+          <div className="mt-1 text-center text-xl">
+            <Answer address={address} loading={loading} result={result}/>
+          </div>
         </div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        By&nbsp;
+        <a className="text-purple-600" href="https://github.com/0xbe1">
+          @0xbe1
+        </a>
+        &nbsp;
+        <a href="https://discord.gg/vN2dTXeNsc">
+          <img src="discord.svg" alt="Discord" className="h-6" />
         </a>
       </footer>
     </div>

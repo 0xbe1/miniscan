de5d1c66148836aba731cbcd334c41a13091f34f
0xbe1 <0xbetrue@gmail.com>
2022-04-08 07:37:53 +0800

    add nader's tweet

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'pbs.twimg.com', // Twitter Profile Picture
+    ],
+  },
 }
